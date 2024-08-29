365d5c04d47cba1ac482d5e6dcb03d1c1558c1e6
Cleo Silva <cleo_silva1906@hotmail.com>
2024-08-29 18:12:14 -0300

    Created Classes

---
+++ src/ContaCorrente.java
@@ -0,0 +1,22 @@
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(){}
+    public void depositar(){}
+    public void transferir(){}
+
+}
