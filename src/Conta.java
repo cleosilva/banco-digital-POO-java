8530e3cae4218a266ca0888e136d363f13341bc0
Cleo Silva <cleo_silva1906@hotmail.com>
2024-08-29 19:11:19 -0300

    Created methods

--- src/Conta.java
+++ src/Conta.java
@@ -1,7 +1,32 @@
-public class Conta {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public abstract class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1 ;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
 
     public int getAgencia() {
         return agencia;
@@ -14,4 +39,11 @@ public class Conta {
     public double getSaldo() {
         return saldo;
     }
+
+    protected void imprimirInfoContas() {
+        System.out.printf("Agência %d%n", this.agencia);
+        System.out.printf("Conta %d%n", this.numero);
+        System.out.printf("Saldo %.2f%n", this.saldo);
+    }
+
 }
--- src/IConta.java
+++ src/IConta.java
@@ -1,5 +1,6 @@
 public interface IConta {
-    void sacar();
-    void depositar();
-    void transferir();
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, Conta contaDestino);
+    void imprimirExtrato();
 }
